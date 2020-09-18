856ac7c11ed589ff5d104816fe052b78593c71b7
trriley <im.triley@gmail.com>
2020-09-18 16:19:55 -0500

    Update process-gtfs.R
    now returns station-specific timetables as JSON files. a LOT of json files. consider filtering down and removing the ones pertaining to stations that are not even on the map

--- r/process-gtfs.R
+++ r/process-gtfs.R
@@ -7,7 +7,8 @@ packages <- c(
   "sf",
   "tidytransit",
   "tidyverse",
-  "here"
+  "here",
+  "jsonlite"
 )
 
 # load packages
@@ -16,9 +17,23 @@ invisible(lapply(packages, library, character.only = TRUE))
 # throws a "parsing failures while reading agency" error,
 # should not significantly impact output
 amtrak <- read_gtfs("./data/transitland/amtrak.zip")
+if(!dir.exists(travelTimesDir <- here("data", "tts"))) {
+  dir.create(travelTimesDir)
+}
+
 departureDate <- "2018-09-04" 
-departureTime <- "06:00:00"
+departureTime <- "00:00:00"
 maxArrivalTime <- "96:00:00"
 stopTimes <- filter_stop_times(amtrak, departureDate, departureTime, maxArrivalTime)
-stopName <- "Chicago Union Station Amtrak"
-travel_times(stopTimes, stopName, 36000)
+# stopName <- "Chicago Union Station Amtrak"
+stopNames <- amtrak$stops$stop_name
+
+for(stopName in stopNames) {
+  tts <- travel_times(stopTimes, stopName, 86400) %>%
+    select(from_stop_id, to_stop_id, travel_time)
+  stopID <- filter(amtrak$stops, stop_name == stopName)[[1,1]]
+  fileName <- paste0(travelTimesDir, "/", stopID, ".json")
+  tts %>%
+    toJSON("columns") %>%
+    write_json(fileName)
+}
