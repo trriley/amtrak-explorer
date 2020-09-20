cd9e1ef99140b8185bd4e68ea2d6a7b5ae58d8b4
Tim Riley <56806401+trriley@users.noreply.github.com>
2020-09-20 00:03:35 -0500

    display travel times
    add button that displays travel times to other stops, add legend, add more metadata, add class breaks and color scheme

--- r/process-gtfs.R
+++ r/process-gtfs.R
@@ -16,12 +16,11 @@ invisible(lapply(packages, library, character.only = TRUE))
 
 # throws a "parsing failures while reading agency" error,
 # should not significantly impact output
-gtfsFile <- paste(here("data", "transitland"), "amtrak.zip", sep = "/")
+gtfsFile <- here("data", "transitland", "amtrak.zip")
 amtrak <- read_gtfs(gtfsFile)
 if(!dir.exists(travelTimesDir <- here("data", "tts"))) {
   dir.create(travelTimesDir)
 }
-fileName <- paste(travelTimesDir, "tts.json", sep = "/")
 
 departureDate <- "2018-09-04" 
 departureTime <- "00:00:00"
@@ -35,7 +34,39 @@ tts <- map_dfr(
   ~ travel_times(stopTimes, stop_name = .x, 86400) %>%
     select(from_stop_id, to_stop_id, travel_time)
 )
-ttsJSON <- tts %>%
-  toJSON("columns")
 
-write_json(ttsJSON, fileName)
+# we have some stations whose only travel_time entry is for themselves, with a
+# value of 0. These aren't terribly useful for our map, so they need to be 
+# removed as well
+# stationsRemove <- tts %>%
+#   group_by(from_stop_id) %>%
+#   summarize(count = n()) %>%
+#   filter(count == 1)
+# 
+# ttsRemoved <- tts %>%
+#   anti_join(stationsRemove)
+
+# semi-join tts with stops.shp to filter both tables
+# (we only want stops.shp to have stations with schedule info, and we only want
+# tts.json to have schedule info for stations in stops.shp)
+stopsGeo <- here("data", "stops_backup.geojson") %>%
+  read_sf() %>%
+  semi_join(tts, by = c("STNCODE" = "from_stop_id"))
+
+write_sf(stopsGeo, here("data", "stops.geojson"))
+
+ttsFiltered <- tts %>%
+  semi_join(stopsGeo, by = c("from_stop_id" = "STNCODE"))
+
+ttsJSON <- list(
+  from_stop_id = ttsFiltered$from_stop_id,
+  to_stop_id = ttsFiltered$to_stop_id,
+  travel_time = ttsFiltered$travel_time
+) %>%
+  toJSON()
+
+jsonFile <- paste(travelTimesDir, "tts.json", sep = "/")
+# write_json(ttsJSON, jsonFile)
+# write_json encodes the data as an array with one element for some insane
+# reason, so it is necessary to manually copy/paste the output of >ttsJSON to
+# the tts.json file
